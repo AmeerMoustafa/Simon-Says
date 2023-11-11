312856210ecb5416c8a476a2799cb615bf174699
Ameer-M <t3hdamnedone@gmail.com>
2023-11-11 14:39:11 +0200

    Refactored from jQuery to vanilla JS

--- scripts/game.js
+++ scripts/game.js
@@ -1,15 +1,16 @@
 // Two arrays to compare results
-let playerClicks = [];
-let requiredClicks = [];
+let player_clicks = [];
+let required_clicks = [];
 
 // Global Variables to select DOM elements
-const document_body = $("body");
-const level_title = $("#level-title");
-const game_buttons = $(".btn");
-const green_btn = $(".green");
-const red_btn = $(".red");
-const yellow_btn = $(".yellow");
-const blue_btn = $(".blue");
+const document_body = document.getElementsByTagName("body")[0];
+const level_title = document.getElementById("level-title");
+const game_buttons = document.querySelectorAll(".btn");
+
+const green_btn = document.getElementsByClassName("green")[0];
+const red_btn = document.getElementsByClassName("red")[0];
+const yellow_btn = document.getElementsByClassName("yellow")[0];
+const blue_btn = document.getElementsByClassName("blue")[0];
 
 // Reusable Helper Functions
 
@@ -17,13 +18,13 @@ const blue_btn = $(".blue");
 const playAudio = (selectedButton) => {
   let audio;
 
-  if (selectedButton.hasClass("green")) {
+  if (selectedButton.classList.contains("green")) {
     audio = new Audio("../sounds/green.mp3");
-  } else if (selectedButton.hasClass("red")) {
+  } else if (selectedButton.classList.contains("red")) {
     audio = new Audio("../sounds/red.mp3");
-  } else if (selectedButton.hasClass("yellow")) {
+  } else if (selectedButton.classList.contains("yellow")) {
     audio = new Audio("../sounds/yellow.mp3");
-  } else if (selectedButton.hasClass("blue")) {
+  } else if (selectedButton.classList.contains("blue")) {
     audio = new Audio("../sounds/blue.mp3");
   }
 
@@ -33,37 +34,46 @@ const playAudio = (selectedButton) => {
 // Handling pressed class toggles
 
 const togglePress = (toToggle) => {
-  const toggleButton = () => toToggle.toggleClass("pressed");
-  toggleButton();
-  setTimeout(toggleButton, 100);
+  const button_toggle = () => toToggle.classList.toggle("pressed");
+
+  button_toggle();
+
+  setTimeout(button_toggle, 100);
+};
+
+// A reusable function to compare the values of two arrays
+
+const compareResults = (player, required) => {
+  return player.toString() === required.toString();
 };
 
 // Level functions
 
 const levelOne = () => {
-  level_title.text("Level 1");
+  level_title.innerText = "Level 1";
   const random_index = Math.floor(Math.random() * game_buttons.length);
-  const selected_button = $(game_buttons[random_index]);
-
-  playAudio(selected_button).play();
+  const selected_button = game_buttons[random_index];
 
   togglePress(selected_button);
+  playAudio(selected_button).play();
 
-  $(`.green, .red, .yellow, .blue`).on("click", (e) => {
-    const clicked_button = $(e.target);
-    togglePress(clicked_button);
-    playAudio(clicked_button).play();
-    const isEqual = $(selected_button).is(e.target);
-    if (isEqual) {
-      console.log("It works!");
-    }
-  });
+  required_clicks.push(selected_button.id);
 };
 
-const gameStart = () => {
-  document_body.on("keypress", () => {
-    levelOne();
+//Binding necessary logic to game buttons
+
+game_buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    const clicked_button = e.target;
+    togglePress(clicked_button);
+    playAudio(clicked_button).play();
+
+    player_clicks.push(clicked_button.id);
   });
+});
+
+const gameStart = () => {
+  document_body.addEventListener("keypress", levelOne);
 };
 
 gameStart();
